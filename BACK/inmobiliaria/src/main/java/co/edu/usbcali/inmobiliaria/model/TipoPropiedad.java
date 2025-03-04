1b31cf8c1ad47a73d1a944dfd13c076672acbcda
CristianF031 <cristianfhd3@gmail.com>
2025-03-03 19:24:32 -0500

    holaa2

--- BACK/inmobiliaria/src/main/java/co/edu/usbcali/inmobiliaria/model/TipoPropiedad.java
+++ BACK/inmobiliaria/src/main/java/co/edu/usbcali/inmobiliaria/model/TipoPropiedad.java
@@ -1,4 +1,31 @@
 package co.edu.usbcali.inmobiliaria.model;
 
 public class TipoPropiedad {
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdTipoPropiedad() {
+        return idTipoPropiedad;
+    }
+
+    public void setIdTipoPropiedad(Integer idTipoPropiedad) {
+        this.idTipoPropiedad = idTipoPropiedad;
+    }
+
+    private Integer idTipoPropiedad;
+    private String nombre;
+    private String descripcion;
 }
